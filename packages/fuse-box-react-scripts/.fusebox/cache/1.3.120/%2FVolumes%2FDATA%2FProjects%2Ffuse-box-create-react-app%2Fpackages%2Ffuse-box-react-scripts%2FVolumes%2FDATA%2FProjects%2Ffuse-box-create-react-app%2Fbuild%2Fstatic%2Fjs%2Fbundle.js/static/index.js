12fa3da3a071485bc8560eb3555f600d83a4d571
Guy Barnard <guy@domabo.com>
2017-03-11 11:05:04 -0600

    removed .fusebox

--- packages/fuse-box-react-scripts/.fusebox/cache/1.3.120/%2FVolumes%2FDATA%2FProjects%2Ffuse-box-create-react-app%2Fpackages%2Ffuse-box-react-scripts%2FVolumes%2FDATA%2FProjects%2Ffuse-box-create-react-app%2Fbuild%2Fstatic%2Fjs%2Fbundle.js/static/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = { contents : "'use strict';\n\nvar _react = require('react');\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = require('react-dom');\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nvar _App = require('./App');\n\nvar _App2 = _interopRequireDefault(_App);\n\nrequire('./index.css');\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('root'));", 
-dependencies : ["react","react-dom","./App","./index.css"], 
-sourceMap : "{\"version\":3,\"sources\":[\"index.js\"],\"names\":[\"render\",\"document\",\"getElementById\"],\"mappings\":\";;AAAA;;;;AACA;;;;AACA;;;;AACA;;;;AAEA,mBAASA,MAAT,CACE,kDADF,EAEEC,SAASC,cAAT,CAAwB,MAAxB,CAFF\",\"file\":\"index.js\",\"sourcesContent\":[\"import React from 'react';\\nimport ReactDOM from 'react-dom';\\nimport App from './App';\\nimport './index.css';\\n\\nReactDOM.render(\\n  <App />,\\n  document.getElementById('root')\\n);\\n\"]}",
-headerContent : undefined, 
-mtime : 1487445891000
-};
